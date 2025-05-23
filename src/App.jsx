c9fb6ad4203d3ce459f2fd67e01ae8f85de72246
NoannNass <92955262+NoannNass@users.noreply.github.com>
2025-05-23 20:33:11 +0200

    configuration de root avec oauth2 keycloak

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import { Button } from "@mui/material";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router } from "react-router";
 import "./App.css";
+import { setCredentials } from "./store/authSlice";
 
 function App() {
+  const { token, tokenData, logIn, LogOut } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  // Removed unused authReady state
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      // setAuthReady removed
+    }
+  }, [token, tokenData, dispatch]);
+
   return (
     <Router>
-      <Button variant="contained" color="#dc004e">
-        LOGIN
-      </Button>
+      {!token ? (
+        <Button
+          variant="contained"
+          color="#dc004e"
+          onClick={() => {
+            logIn();
+          }}
+        >
+          LOGIN
+        </Button>
+      ) : (
+        <div>
+          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+        </div>
+      )}
     </Router>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { AuthProvider } from "react-oauth2-code-pkce";
 import { Provider } from "react-redux";
+import { authConfig } from "./authConfig";
 import { store } from "./store/store";
 
 import App from "./App";
@@ -9,7 +10,12 @@ import App from "./App";
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <AuthProvider
+    authConfig={authConfig}
+    loadingComponent={<div>Loading...</div>}
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthProvider>
 );
